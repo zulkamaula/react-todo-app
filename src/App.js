c36696de180c93c625e39bb3f886a1c14d6d7690
Zulkariski Mauladi <74296102+zulkamaula@users.noreply.github.com>
2022-08-04 06:26:04 +0000

    Completing a CRUD Todo List in React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import TodoInput from "./components/TodoInput";
+import TodoItem from "./components/TodoItem";
 
 function App() {
+  const [todoItems, setTodoItems] = React.useState(
+    [
+      {
+        todo: 'Mow the lawn',
+        complete: false
+      },
+      {
+        todo: 'Do Laundry',
+        complete: false
+      },
+      {
+        todo: 'Make Dinner',
+        complete: false
+      }
+    ])
+
+  const createTodoItem = (todo) => {
+    const newTodoItems = [...todoItems, { todo, complete: false }];
+    setTodoItems(newTodoItems);
+  };
+
+  const deleteTodoItem = (index) => {
+    const newTodoItems = [...todoItems]
+    newTodoItems.splice(index, 1)
+    setTodoItems(newTodoItems)
+  }
+
+  const completeTodoItem = (index) => {
+    const newTodoItems = [...todoItems];
+    newTodoItems[index].complete === false
+    ? (newTodoItems[index].complete = true)
+    : (newTodoItems[index].complete = false);
+    setTodoItems(newTodoItems)
+  };
+
+  const updateTodoItem = (index) => {
+    const newTodoItems = [...todoItems];
+    const item = newTodoItems[index];
+    let newItem = prompt(`Update ${item.todo}?`, item.todo);
+    let todoObj = { todo: newItem, complete: false };
+    
+    newTodoItems.splice(index, 1, todoObj);
+    
+    if (newItem === null || newItem === "") {
+      return;
+    } else {
+      item.todo = newItem;
+    }
+    setTodoItems(newTodoItems);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <TodoInput createTodoItem={createTodoItem} />
+      {
+        todoItems.map((item, index) => (
+          <TodoItem 
+            key={index} 
+            index={index} 
+            item={item} 
+            deleteTodoItem={deleteTodoItem}
+            completeTodoItem={completeTodoItem}
+            updateTodoItem={updateTodoItem} />
+        ))
+      }
     </div>
   );
 }
